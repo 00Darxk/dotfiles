2daf1d43ba2b176940da624b69d8ba75a7c4364c
00Darxk <gsturm2003@gmail.com>
2026-08-19 13:58:41 +0200

    update tailscale module

--- waybar/scripts/tailscaleinfo.sh
+++ waybar/scripts/tailscaleinfo.sh
@@ -1,49 +1,55 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Set your hostname in the appropriate file
 # disable in waybar if not needed
 
-hostnames=($(cat "$HOME/.config/.secrets/hostnames.txt")) 
-sshhost=($(cat "$HOME/.config/.secrets/hostname.txt")) 
+hostnames=($(cat "$HOME/.config/.secrets/hostnames.txt"))
+sshhost=($(cat "$HOME/.config/.secrets/hostname.txt"))
 
-if [ -z $hostnames ] ; then
-    hostnames=$sshhost
+if [ -z $hostnames ]; then
+  hostnames=$sshhost
 fi
 
 for i in "${!hostnames[@]}"; do
-    hostname="${hostnames[$i]}"
+  hostname="${hostnames[$i]}"
 
-    ip=$(tailscale ip -4 "$hostname")
-    status=$(tailscale status | awk -v h="$hostname" '$0 ~ h {print $NF}')
+  ip=$(tailscale ip -4 "$hostname")
+  status=$(tailscale ping -c 1 --until-direct=false --timeout=1s "$hostname" >/dev/null 2>&1 || echo $?)
 
-    if [ "$status" = "offline" ]; then
-        if [ "$sshhost" = "$hostname" ]; then
-            css_class=red
-        fi
-        status_icon=""
-    else
-        css_class=green
-        status_icon=""
-    fi
-
+  if ! [ -z "$status" ]; then
     if [ "$sshhost" = "$hostname" ]; then
-        text+=">  <span foreground = \"${css_class}\">${hostname}: ${ip} ${status_icon} </span>"
-    else
-        text+="   <span foreground = \"${css_class}\">${hostname}: ${ip} ${status_icon} </span>"
+      css_class=red
     fi
+    css_class_hostname=red
+    status_icon=""
+  else
+    if [ "$sshhost" = "$hostname" ]; then
+      css_class=green
+    fi
+    css_class_hostname=green
+    status_icon=""
+  fi
 
-    # dumb way to do this, i'm tired don't judge me ;_;
-    let "j=i+1"
-    if [ $j -lt ${#hostnames[@]} ]; then
-        text+=$'\n'
-    fi
+  if [ "$sshhost" = "$hostname" ]; then
+    text+=">  <span foreground = \"${css_class_hostname}\">${hostname}: ${ip} ${status_icon} </span>"
+  else
+    text+="   <span foreground = \"${css_class_hostname}\">${hostname}: ${ip} ${status_icon} </span>"
+  fi
+
+  # dumb way to do this, i'm tired don't judge me ;_;
+  let "j=i+1"
+  if [ $j -lt ${#hostnames[@]} ]; then
+    text+=$'\n'
+  fi
 done
 
 jq -nc \
-        --arg text "$text" \
-        --arg tooltip ""\
-        --arg class "$css_class" \
-        '{
+  --arg text "$text" \
+  --arg tooltip "" \
+  --arg class "$css_class" \
+  '{
             text: $text,
             tooltip: $tooltip,
             class: $class
